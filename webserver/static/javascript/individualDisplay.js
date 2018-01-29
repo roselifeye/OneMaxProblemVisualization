1f61c08414542e4c9317b017dcdc99f93dd145ed
roselifeye <roselifeye@gmail.com>
2018-01-29 19:34:37 -0330

    no message

--- webserver/static/javascript/individualDisplay.js
+++ webserver/static/javascript/individualDisplay.js
@@ -5,7 +5,7 @@ var individualChart = echarts.init(document.getElementById('box1'));
 var genepoints = Array.apply(null, Array(20)).map(function(item, i) {
   return i + 1;
 });
-var individuals = Array.apply(null, Array(50)).map(function(item, i) {
+var individuals = Array.apply(null, Array(100)).map(function(item, i) {
   return i + 1;
 });
 var indData = [];
@@ -69,6 +69,12 @@ function initIndChart() {
         show: false
       }
     },
+    dataZoom: [{ // 这个dataZoom组件，默认控制x轴。
+      type: 'slider', // 这个 dataZoom 组件是 slider 型 dataZoom 组件
+      orient:"vertical",
+      start: 0, // 左边在 10% 的位置。
+      end: 20 // 右边在 50% 的位置。
+    }],
     animation:false,
     series: [{
       name: 'One',
